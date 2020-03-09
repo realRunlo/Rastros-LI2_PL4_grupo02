662eef6fddae0e92b8b954f5ea0540b8f3eabb15
brazafonso <61985783+brazafonso@users.noreply.github.com>
2020-03-09 20:22:16 +0000

    modulo interpretador.c

--- interpretador.c
+++ interpretador.c
@@ -6,17 +6,6 @@
 #include <string.h>
 #include "camada_dados.h"
 
-void desenha_tabuleiro(ESTADO e){
-    for(int l = 0; l < 8; l++){
-        for(int c = 0; c < 8; c++) {
-            if (e.tab[l][c] == BRANCA) printf("*");
-            else if (c == 0 && l == 7) printf("1");
-                else if (c == 7 && l == 0) printf("2");
-                    else if (e.tab[l][c] == PRETA) printf("#");
-                        else printf(".");
-        }
-        printf("\n");
-    }
-}
+
 
 
