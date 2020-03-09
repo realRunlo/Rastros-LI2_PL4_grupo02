e02704de7eadbbc3ae3acb1b620c0624ecf31e55
brazafonso <61985783+brazafonso@users.noreply.github.com>
2020-03-09 20:21:07 +0000

    funcao que desenha o tabuleiro

---
+++ interpretador.h
@@ -0,0 +1,31 @@
+//
+// Created by runlo on 09/03/20.
+//
+
+#ifndef ___CAMADADEDADOS_H___
+#define ___CAMADADEDADOS_H___
+
+// conjunto de tipos utilizados para armazenamento e edicao dos dados do estado do jogo
+
+typedef enum {VAZIO, BRANCA, PRETA} CASA;
+typedef struct {
+    int coluna;
+    int linha;
+} COORDENADA;
+typedef struct {
+    COORDENADA jogador1;
+    COORDENADA jogador2;
+} JOGADA;
+typedef JOGADA JOGADAS[32];
+typedef struct{
+    CASA tab[8][8];
+    COORDENADA ultima_jogada;
+    JOGADAS jogadas;
+    int num_jogadas;
+    int jogador_atual;
+} ESTADO;
+
+// //////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+#endif //___CAMADADEDADOS_H__
