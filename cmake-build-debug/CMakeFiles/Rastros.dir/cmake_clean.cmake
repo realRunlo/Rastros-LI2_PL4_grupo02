d96e69c5bb2e873afa2f3c9c9f646987e35eaa32
realrunlo <gteixeira.pereira@gmail.com>
2020-03-10 20:16:21 +0000

    tabuleiro numerado.

---
+++ cmake-build-debug/CMakeFiles/Rastros.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Rastros.dir/camada_dados.c.o"
+  "CMakeFiles/Rastros.dir/interpretador.c.o"
+  "CMakeFiles/Rastros.dir/logica.c.o"
+  "CMakeFiles/Rastros.dir/rastros.c.o"
+  "Rastros"
+  "Rastros.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Rastros.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
