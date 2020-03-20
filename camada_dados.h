79d169b3f6389cf176db4d11817a8ef6868ed01f
realrunlo <gteixeira.pereira@gmail.com>
2020-03-20 17:03:30 +0000

    documentação dados.h

--- camada_dados.h
+++ camada_dados.h
@@ -4,8 +4,7 @@
 
 /**
 @file dados.h
-Definição do estado e das funções que o manipulam
-*/
+Definição do estado e das funções que o manipulam*/
 
 #ifndef ___CAMADADEDADOS_H___
 #define ___CAMADADEDADOS_H___
@@ -39,63 +38,134 @@ typedef JOGADA JOGADAS[32];
 \brief Tipo de dados para o estado
 */
 typedef struct{
-    /** O tabuleiro */
+    /** O tabuleiro*/
     CASA tab[8][8];
     /** Coordenada da ultima jogada*/
     COORDENADA ultima_jogada;
-    /** Jogadas efetuadas */
+    /** Jogadas efetuadas*/
     JOGADAS jogadas;
     /** Número de jogadas*/
     int num_jogadas;
+    /** Jogador atual*/
     int jogador_atual;
+    /** Número de comandos excutados*/
     int num_comandos;
 } ESTADO;
 
 // //////////////////////////////////////////////////////////////////////////////////////////////////////
-
+/** \brief Inicializa o estado*/
 ESTADO *inicializar_estado(); // funcao que da o estado inicial do jogo
 
+/** \brief Converte um número para a letra correspondente
+ * @param x Número a converter*/
 char converte_numero(int x); // funcao que torna um numero de uma coluna numa letra
 
+/** \brief Verifica se uma casa tem peca branca
+ * @param e Apontador para o estado
+ * @param l Linha da casa
+ * @param c Coluna da casa*/
 int e_branca(ESTADO *e, int l, int c); // funcao que verifica se uma casa tem peca branca
 
+/** \brief Verifica se uma casa é preta
+ * @param e Apontador para o estado
+ * @param l Linha da casa
+ * @param c Coluna da casa*/
 int e_preta(ESTADO *e, int l, int c); // funcao que verifica se uma casa tem peca preta
 
+
+/** \brief Verifica se uma casa é vazia
+ * @param e Apontador para o estado
+ * @param l Linha da casa
+ * @param c Coluna da casa*/
 int e_vazio(ESTADO *e, int l, int c); // funcao que verifica se uma casa tem peca vazio
 
+/** \brief Altera uma casa para branca
+ * @param e Apontador para o estado
+ * @param l Linha da casa
+ * @param c Coluna da casa*/
 void set_branca(ESTADO *e, int l, int c); // funcao que torna uma casa numa peca branca
 
+/** \brief Altera uma casa para preta
+ * @param e Apontador para o estado
+ * @param l Linha da casa
+ * @param c Coluna da casa*/
 void set_preta(ESTADO *e, int l, int c); // funcao que torna uma casa numa peca preta
 
+/** \brief Altera uma casa para vazia
+ * @param e Apontador para o estado
+ * @param l Linha da casa
+ * @param c Coluna da casa*/
 void set_vazio(ESTADO *e, int l, int c); // funcao que torna uma casa numa peca vazio
 
+/** \brief Retorna o jogador atual
+ * @param e Apontador para o estado*/
 int get_jogador(ESTADO *e); //funcao que retorna o numero do jogador atual
 
-int set_jogador(ESTADO *e, int j); //funcao que atualiza o numero do jogador atual
-
+/** \brief Retorna o número de jogadas
+ * @param e Apontador para o estado*/
 int get_Njogadas(ESTADO *e);//retorna o número de jogadas
 
+/** \brief Retorna o número de comandos
+ * @param e Apontador para o estado*/
 int get_Ncomandos(ESTADO *e);//retorna o número de comandos
 
-int set_nJogadas(ESTADO *e, int nJ);//atualiza o número de jogadas
-
-int set_nComandos(ESTADO *e, int nJ);//atualiza o número de comandos
-
+/** \brief Retorna o número da coluna de uma coordenada
+ * @param e Apontador para o estado*/
 int get_coluna(COORDENADA c); // retorna o número da coluna de uma coordenada
 
+/** \brief Retorna o número da linha de uma coordenada
+ * @param e Apontador para o estado*/
 int get_linha(COORDENADA c); // retorna o número da linha de uma coordenada
 
+/** \brief Altera o numero do jogador atual
+ * @param e Apontador para o estado
+ * @param j Jogador anterior*/
+int set_jogador(ESTADO *e, int j); //funcao que atualiza o numero do jogador atual
 
+/** \brief Altera o número de jogadas
+ * @param e Apontador para o estado
+ * @param j Número de jogadas anterior*/
+int set_nJogadas(ESTADO *e, int nJ);//atualiza o número de jogadas
 
-void adiciona_lista_jogadas(ESTADO *e, int l, int c, int jogador); // funcao que adiciona na lista de jogadas a ultima jogada feita
+/** \brief Altera o número de comandos
+ * @param e Apontador para o estado
+ * @param nj Número de comandos anterior*/
+int set_nComandos(ESTADO *e, int nJ);//atualiza o número de comandos
 
+/** \brief Altera as coordenadas da ultima jogada
+ * @param e Apontador para o estado
+ * @param l Linha da casa da jogada
+ * @param c Coluna da casa da jogada*/
 void set_ultima_jogada(ESTADO *e, int l, int c); //Funcao que modifica as coordenadas da ultima jogada
 
+
+/** \brief Adiciona na lista de jogadas a ultima jogada feita
+ * @param e Apontador para o estado
+ * @param l Linha da jogada
+ * @param c Coluna da jogada
+ * @param jogador Jogador que efetuou a jogada*/
+void adiciona_lista_jogadas(ESTADO *e, int l, int c, int jogador); // funcao que adiciona na lista de jogadas a ultima jogada feita
+
+
+/** \brief Adiciona 1 ao numero de jogadas
+ * @param e Apontador para o estado*/
 void add_numjogadas(ESTADO *e); //Adiciona 1 ao numero de jogadas
 
+/** \brief Adiciona 1 ao numero de comandos
+ * @param e Apontador para o estado*/
 void add_numcomandos(ESTADO *e); //Adiciona 1 ao numero de comandos
 
+/** \brief Atualiza o tabuleiro no estado de acordo com o que esta gravado no ficheiro externo
+ * @param e Apontador para o estado
+ * @param l Linha da jogada
+ * @param c Coluna da jogada
+ * @param x Caracter nessa casa*/
 void novo_tabuleiro(ESTADO *e, int l, int c, char x); // atualiza o tabuleiro no estado de acordo com o que esta gravado no ficheiro de texto
 
+/** \brief Atualiza o estado a partir do prompt do que esta gravado no ficheiro externo
+ * @param e Apontador para o estado
+ * @param jogador Jogador atual do prompt
+ * @param nJogadas Número de jogadas
+ * @param nComandos Número de jogadas*/
 void novo_prompt(ESTADO *e, int jogador, int nJogadas, int nComandos); // atualiza o estado a partir do prompt do que esta gravado no ficheiro
 #endif //___CAMADADEDADOS_H__
--- html/search/all_2.js
+++ html/search/all_2.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['jogada',['JOGADA',['../structJOGADA.html',1,'']]],
-  ['jogadas',['jogadas',['../structESTADO.html#afae43b87a488fad0f2b56a18bad31d18',1,'ESTADO']]]
+  ['jogadas',['jogadas',['../structESTADO.html#afae43b87a488fad0f2b56a18bad31d18',1,'ESTADO']]],
+  ['jogador_5fatual',['jogador_atual',['../structESTADO.html#a5dd28e2e68b7aef2b6b7ea88e02eff58',1,'ESTADO']]]
 ];
