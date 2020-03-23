0e52886df88778aed16c7d1dace8be051a385534
realrunlo <gteixeira.pereira@gmail.com>
2020-03-23 12:23:00 +0000

    Mudança função ler e gravar

--- interface.c
+++ interface.c
@@ -41,16 +41,13 @@ void imprime_estadoL(ESTADO *e){ //prompt do ler
 // FUNÇÔES DE MANUSEAMENTO DE FICHEIRO EXTERNO ///////////////////////////////////////////////////////////////
 
 void grava_tabuleiro(ESTADO *e,FILE *filename){ // Função que grava o tabuleiro num dado ficheiro
-    fprintf (filename,"   A B C D E F G H \n");
-    fprintf (filename,"   _ _ _ _ _ _ _ _ \n");
     for(int l = 7; l >= 0; l--){
-        fprintf(filename,"%d| ", l+1 );  //depois por (l+1)
         for(int c = 0; c < 8; c++) {
-            if (e_branca(e , l ,c)) fprintf(filename,"* ");
-            else if (c == 0 && l == 0) fprintf(filename,"1 ");
-            else if (c == 7 && l == 7) fprintf(filename,"2 ");
-            else if (e_preta(e , l ,c)) fprintf(filename,"# ");
-            else fprintf(filename,". ");
+            if (e_branca(e , l ,c)) fprintf(filename,"*");
+            else if (c == 0 && l == 0) fprintf(filename,"1");
+            else if (c == 7 && l == 7) fprintf(filename,"2");
+            else if (e_preta(e , l ,c)) fprintf(filename,"#");
+            else fprintf(filename,".");
         }
         fprintf(filename,"\n");
     }
@@ -67,7 +64,6 @@ void gravar(ESTADO *e,const char *filename, const char *mode){
 
     fp = fopen(filename,mode);
     grava_tabuleiro(e,fp);
-    grava_estado(e,fp);
 
     fclose(fp);
 }
@@ -83,23 +79,14 @@ void ler(ESTADO *e, const char *filename, const char *mode){
 
     fp = fopen(filename,mode);
 
-    char c;
-    int l = 7, co = 0; //posição do canto superior esquerdo do tabuleiro
-    for(int lTab=10;lTab>0;lTab--){ //lê cada linha do tabuleiro , acontagem das linhas vai de cima para baixo por uma questão de correspondência à imagem
-        co = 0;
-        for(int cTab=0;cTab<=19;cTab++){
-            fscanf(fp,"%c",&c);
-            if((c=='*' || c=='#' || c=='.' || c=='2' || c=='1') && cTab>3){
-                novo_tabuleiro(e, l, co, c); // atualiza o estado do tabuleiro de acordo com o gravado no ficheiro
-                co++;
-            }
+    char crt;
+    int l,c ; //posição do canto superior esquerdo do tabuleiro
+    for(l=7;l>0;l--){ //lê cada linha do tabuleiro , a contagem das linhas vai de cima para baixo por uma questão de correspondência à imagem
+        for(c=0;c<=8;c++){
+            fscanf(fp,"%c",&crt);
+            novo_tabuleiro(e,l,c,crt);
         }
-        if (lTab <=8) l--;
     }
-
-    int nComandos, jogador, nJogadas;
-    fscanf(fp,"#  %d Jog:%d N:%d",&nComandos, &jogador, &nJogadas); //lê as informações do prompt
-    novo_prompt(e, jogador, nJogadas, nComandos); // atualiza o estado com a info do novo prompt
 
     fclose(fp);
 }
@@ -134,7 +121,7 @@ int interpretador(ESTADO *e) {
         else if (strlen(linha) == 4 && sscanf(linha,"%c%c%c",&c1,&c2,&c3) == 3 && c1=='l' && c2=='e' && c3=='r'){
             ler(e, db, "r");  //lê o novo tabuleiro atualizando o respetivo estado
             desenha_tabuleiro(e);   //desenha um novo tabuleiro
-            imprime_estadoL(e);
+            //imprime_estadoL(e);
 
         }
 
@@ -151,7 +138,7 @@ int interpretador(ESTADO *e) {
 }
 
 
-// ////////////////////////  WORK IN PROGRESS //////////////////////////////////////////////
+// //////////////////////// Listar movimentos //////////////////////////////////////////////
 
 void lista_ronda(ESTADO *e,int i){
     if(i==get_Njogadas(e)-1)
